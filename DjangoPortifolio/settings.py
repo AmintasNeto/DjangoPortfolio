e66148845816546b5dea4953aab763703eda22ca
AmintasNeto <netorubok14@gmail.com>
2023-12-27 21:06:48 -0300

    Fix static files bug

--- DjangoPortifolio/settings.py
+++ DjangoPortifolio/settings.py
@@ -116,7 +116,7 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = "/images/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
